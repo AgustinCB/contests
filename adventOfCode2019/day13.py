c99242da28d8240d06b628e09c19a70952afa7ed
Agustin Chiappe Berrini <agustin@chiappeberrini.me>
2022-03-18 05:58:27 -0700

    Day 13
    Man, what a ride.

--- adventOfCode2019/day13.py
+++ adventOfCode2019/day13.py
@@ -1,6 +1,6 @@
 import sys
-from collections import defaultdict
-from typing import List, Optional, Tuple, Dict
+from collections import defaultdict, Counter
+from typing import Optional, List, Tuple, Dict
 
 
 class IntCodeMachine(object):
@@ -13,35 +13,33 @@ class IntCodeMachine(object):
         self.ip = 0
         self.relative_base = 0
 
-    def fetch_value(self, index: int, mode: int) -> int:
+    def fetch_index(self, index: int, mode: int) -> int:
         if mode == 1:
-            return self.program[index]
+            return index
         elif mode == 0:
-            return self.program[self.program[index]]
+            return self.program[index]
         elif mode == 2:
-            return self.program[self.relative_base + self.program[index]]
+            return self.relative_base + self.program[index]
         else:
             raise RuntimeError("Invalid fetch mode {}", mode)
 
-    def addition(self, index: int, modes: Tuple[int, int], relative_output: bool):
+    def fetch_output_index(self, relative_output: bool, index: int) -> int:
         if relative_output:
-            self.program[self.relative_base + self.program[index + 3]] = self.fetch_value(index + 1,
-                                                                                          modes[0]) + self.fetch_value(
-                index + 2,
-                modes[1])
+            return self.relative_base + self.program[index]
         else:
-            self.program[self.program[index + 3]] = self.fetch_value(index + 1, modes[0]) + self.fetch_value(index + 2,
-                                                                                                             modes[1])
+            return self.program[index]
+
+    def fetch_value(self, index: int, mode: int) -> int:
+        index = self.fetch_index(index, mode)
+        return self.program[index]
+
+    def addition(self, index: int, modes: Tuple[int, int], relative_output: bool):
+        output_index = self.fetch_output_index(relative_output, index + 3)
+        self.program[output_index] = self.fetch_value(index + 1, modes[0]) + self.fetch_value(index + 2, modes[1])
 
     def multiplication(self, index: int, modes: Tuple[int, int], relative_output: bool):
-        if relative_output:
-            self.program[self.relative_base + self.program[index + 3]] = self.fetch_value(index + 1,
-                                                                                          modes[0]) * self.fetch_value(
-                index + 2,
-                modes[1])
-        else:
-            self.program[self.program[index + 3]] = self.fetch_value(index + 1, modes[0]) * self.fetch_value(index + 2,
-                                                                                                             modes[1])
+        output_index = self.fetch_output_index(relative_output, index + 3)
+        self.program[output_index] = self.fetch_value(index + 1, modes[0]) * self.fetch_value(index + 2, modes[1])
 
     def peek_instruction(self):
         return self.program[self.ip] % 100
@@ -58,12 +56,8 @@ class IntCodeMachine(object):
             self.multiplication(self.ip, (param1_mode, param2_mode), relative_output)
             self.ip += 4
         elif opcode == 3:
-            if param1_mode == 0:
-                self.program[self.program[self.ip + 1]] = self.inputs.pop()
-            elif param1_mode == 1:
-                self.program[self.ip + 1] = self.inputs.pop()
-            elif param1_mode == 2:
-                self.program[self.relative_base + self.program[self.ip + 1]] = self.inputs.pop()
+            index = self.fetch_index(self.ip + 1, param1_mode)
+            self.program[index] = self.inputs.pop()
             self.ip += 2
         elif opcode == 4:
             value = self.fetch_value(self.ip + 1, param1_mode)
@@ -118,6 +112,12 @@ class IntCodeMachine(object):
             self.run_next()
         return self.outputs
 
+    def run_until_input(self) -> Optional[int]:
+        code = self.run_next()
+        while code != 3 and self.can_continue():
+            code = self.run_next()
+        return self.program[self.ip]
+
     def run_until_output(self) -> Optional[int]:
         code = self.run_next()
         while code != 4 and self.can_continue():
@@ -131,59 +131,75 @@ class IntCodeMachine(object):
         return [int(c) for c in input_program.strip().split(",")]
 
 
-DIRECTIONS = [
-    (-1, 0),
-    (0, 1),
-    (1, 0),
-    (0, -1),
-]
+def print_map(output: List[int], screen_map: Dict[Tuple[int, int], int]) -> Tuple[int, Tuple[int, int], Tuple[int, int]]:
+    screen = []
+    in_seventh = []
+    ball = None
+    paddle = None
+    for i in range(len(output) // 3):
+        x, y, tile_id = output[i * 3], output[i * 3 + 1], output[i * 3 + 2]
+        screen.append((x, y, tile_id))
+    height = 24
+    width = 44
+    for (x, y, t) in screen:
+        screen_map[(x, y)] = t
+    for ((x, y), tile_id) in screen_map.items():
+        if (y == 17 or y == 16) and tile_id == 2:
+            in_seventh.append((y, x))
+    for y in range(height):
+        screen_line = ""
+        for x in range(width):
+            tile = None
+            if screen_map[(x, y)] == 0:
+                tile = " "
+            elif screen_map[(x, y)] == 1:
+                tile = "x"
+            elif screen_map[(x, y)] == 2:
+                tile = "@"
+            elif screen_map[(x, y)] == 3:
+                tile = "-"
+                paddle = (x, y)
+            elif screen_map[(x, y)] == 4:
+                tile = "o"
+                ball = (x, y)
+            screen_line += tile
+        print(screen_line)
+    print()
+    print("SCORE", screen_map[(-1, 0)], "BALL", ball, "PADDLE", paddle)
+    print()
+    return screen_map[(-1, 0)], ball, paddle
 
 
-def robot(brain: IntCodeMachine, starting_color: int) -> Dict[Tuple[int, int], int]:
-    painted = set()
-    colors = defaultdict(int)
-    current_position = (0, 0)
-    colors[current_position] = starting_color
-    direction = 1
-    iterations = 0
-    while brain.can_continue():
-        brain.inputs = [colors[current_position]]
-        color = brain.run_until_output()
-        if color is None:
-            break
-        if color not in [1, 0]:
-            raise RuntimeError("Unexpected color: {}".format(color))
-        colors[current_position] = color
-        painted.add(current_position)
-        turn = brain.run_until_output()
-        if turn == 0:
-            direction = (direction - 1) % len(DIRECTIONS)
-        elif turn == 1:
-            direction = (direction + 1) % len(DIRECTIONS)
-        else:
-            raise RuntimeError("Unexpected direction: {}".format(direction))
-        current_position = (
-            current_position[0] + DIRECTIONS[direction][0], current_position[1] + DIRECTIONS[direction][1])
-        iterations += 1
-    return colors
+def destroy_all_balls(code: List[int]) -> int:
+    machine = IntCodeMachine(code, [0])
+    screen = {}
+    machine.run_until_input()
+    print_map(machine.outputs, screen)
+    for ((x, y), i) in screen.items():
+        if i != 2:
+            continue
+        machine.program[388] = x
+        machine.program[389] = y + 1
+        machine.program[390] = 0
+        machine.program[391] = -1
+        machine.program[2753] = x
+        machine.program[2754] = y
+        machine.inputs = [0]
+        machine.run_until_input()
+        print_map(machine.outputs, screen)
+    return machine.program[386]
 
 
 def solve():
-    machine = IntCodeMachine(IntCodeMachine.parse_string(sys.stdin.readline()), [])
     if sys.argv[1] == 'part1':
-        return len(robot(machine, 0))
+        machine = IntCodeMachine(IntCodeMachine.parse_string(sys.stdin.readline()), [])
+        machine.run()
+        return len([tile_id for (i, tile_id) in enumerate(machine.outputs) if i % 3 == 2 and tile_id == 2])
     if sys.argv[1] == 'part2':
-        colors = robot(machine, 1)
-        message_positions = sorted(list(colors))
-        width = max(i[0] for i in message_positions) + 1
-        height = max(abs(i[1]) for i in message_positions) + 1
-        message = ''
-        for y in range(height):
-            row = ''
-            for x in range(width):
-                row += '#' if (x, -y) in colors and colors[(x, -y)] == 1 else '.'
-            message += row + '\n'
-        return message
+        with open("inputs/day13input.txt", "r") as f:
+            p = IntCodeMachine.parse_string(f.readline())
+        p[0] = 2
+        return destroy_all_balls(p)
 
 
 if __name__ == '__main__':
